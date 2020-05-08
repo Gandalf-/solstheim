404193593b11ee8b19bcf8410d3efec28cb941f6
Gandalf- <austin.voecks@gmail.com>
2020-05-07 17:28:26 -0700

    persistent state

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 Reddit bot
 '''
 
+import json
+import sys
 import datetime
 import praw
 import bot_secrets
@@ -21,24 +23,72 @@ msg = (
 
 now = datetime.datetime.now
 
+class State():
+    ''' persistant state '''
+
+    def __init__(self, filename='state.json'):
+        self.filename = filename
+        self.state = self._load()
+
+    def __getitem__(self, key):
+        return self.state[key]
+
+    def __setitem__(self, key, value):
+        self.state[key] = value
+
+    def _load(self):
+        ''' load data '''
+        with open(self.filename, 'r') as fd:
+            return json.load(fd)
+
+    def save(self):
+        ''' persist '''
+        with open(self.filename, 'w+') as fd:
+            json.dump(self.state, fd)
+
+    def setdefault(self, key, value):
+        ''' dict behavior '''
+        if key not in self.state:
+            self.state[key] = value
+
 def main():
     ''' read comments, distribute snark
     '''
     morrowind = reddit.subreddit('Morrowind')
-    stats = {'read': 0, 'matched': 0}
+
+    state = State()
+    seen = 'submissions_seen'
+    stats = 'statistics'
+
+    state.setdefault(seen, [])
+    state.setdefault(stats, {'read': 0, 'matched': 0, 'skipped': 0})
+    print('starting', state[stats])
 
     for comment in morrowind.stream.comments(skip_existing=True):
 
-        if 'solstheim' in comment.body.lower():
-            stats['matched'] += 1
+        state[stats]['read'] += 1
+
+        if state[stats]['read'] % 100 == 0:
+            print(now(), state[stats])
+            state.save()
+
+        if 'solstheim' not in comment.body.lower():
+            continue
+
+        if comment.submission.id in state[seen]:
+            # don't comment on the same submission twice
+            print(now(), 'skipping', comment.permalink)
+            state[stats]['skipped'] += 1
+
+        else:
             print(now(), 'replying to', comment.permalink)
             comment.reply(msg)
 
-        stats['read'] += 1
+            state[seen].append(comment.submission.id)
+            state[stats]['matched'] += 1
 
-        if stats['read'] % 100 == 0:
-            print(now(), stats)
+        state.save()
 
 
-if __name__ == '__main__':
+if not sys.flags.interactive and __name__ == '__main__':
     main()
